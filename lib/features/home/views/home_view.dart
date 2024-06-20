0d2e9f2b4dd91ed344a635f2e3422629b1b09c5d
bayudss <bayu@dihardjasoftware.com>
2024-06-20 20:28:08 +0700

    june and go_router setup

--- lib/features/home/views/home_view.dart
+++ lib/features/home/views/home_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:june/june.dart';
+import 'package:personal_habit_tracker/features/home/viewmodels/home_viewmodel.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -8,35 +11,42 @@ class HomeView extends StatelessWidget {
     return Scaffold(
       body: SizedBox(
         child: CustomScrollView(
+          physics: const ClampingScrollPhysics(),
           slivers: <Widget>[
             SliverAppBar(
               stretch: true,
-              expandedHeight: 100,
-              backgroundColor: Colors.blue,
-              flexibleSpace: FlexibleSpaceBar(
-                title: Text("This is the title"),
-                background: FlutterLogo(),
+              expandedHeight: 100.h,
+              backgroundColor: Colors.white,
+              flexibleSpace: const FlexibleSpaceBar(
+                title: Text("Personal Habit Tracker"),
               ),
+              forceElevated: true,
+              shadowColor: Colors.black,
             ),
             SliverToBoxAdapter(
-              child: Column(
-                children: [
-                  ...List.generate(20, (index) => Text("Wasdwdsd")),
-                ],
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: JuneBuilder(
+                  () => HomeVM(context: context),
+                  builder: (vm) => const Column(
+                    children: [
+                      Text(
+                        "Looks like you don't have any habit added into the list,\nlet's add them now!",
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
               ),
-            )
-            // SliverList(
-            //   delegate: SliverChildBuilderDelegate(
-            //     (BuildContext context, int index) {
-            //       return Container(
-            //         child: Text("Text"),
-            //       );
-            //     },
-            //     childCount: 20,
-            //   ),
-            // ),
+            ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed:
+            June.getState(() => HomeVM(context: context)).navigateToAddHabit,
+        shape: const CircleBorder(),
+        child: const Icon(Icons.add),
       ),
     );
   }
